929583eac1dcbd8154ebef0539399f59cca049b3
SuperMeerkat345 <artiom22903@gmail.com>
2024-06-29 17:09:07 -0400

    started integrating the html into the js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var map = {};
 window.addEventListener('keydown', (keyPressed) => {
     switch(keyPressed.key){
         case "~":
-            if(map.Control === true){
-               var w = window.open('https://3785216c-b450-430b-811b-5c449a428375-00-3f04o3vth56c2.picard.replit.dev/', "ModWindow", "width=600, height=475, scrollbars=no");
+            if(map.Control === true && map.Shift === true){
+                //https://3785216c-b450-430b-811b-5c449a428375-00-3f04o3vth56c2.picard.replit.dev/
+               var w = window.open('file:///C:\Users\Admin\AlexFinger\EducationEnhanced\window.html', "ModWindow", "width=600, height=475, scrollbars=no");
+               
             }
             break;
         case "Control":
