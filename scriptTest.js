33726deda503af56a6c907dd6da130b4012a9a64
SuperMeerkat345 <artiom22903@gmail.com>
2024-06-30 23:49:09 -0400

    FINALLY FIGURED OUT HOW TO GET AROUND CSP, put everything on one page and attached all links to the ids of the different parts

--- scriptTest.js
+++ scriptTest.js
@@ -1,19 +1,132 @@
 var newHTML = 
 `
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Education Enhanced</title>
+
+  <style>
+* {
+  margin: 0px;
+  padding: 0px;
+  overflow: hidden;
+}
+
+html {
+  height: 400px;
+  width: 600px;
+}
+
+body {
+  background: black;
+  color: white;
+}
+
+.screen {
+  margin-bottom: 1000px;
+}
+
+.nav {
+  border-bottom: solid white 2px;
+  padding: 5px;
+}
+
+.nav li {
+  list-style-type: none;
+  display: inline-block;
+  margin-left: 4px;
+}
+
+.nav a {
+  text-decoration: none;
+  color: blue;
+}
+
+.content {
+  margin-left: 7px;
+}
+
+.calcNav a{
+  text-decoration: none;
+  color: blue;
+}
+
+.content a {
+  text-decoration: none;
+  color: blue;
+}
+  </style>
+
 </head>
+
 <body>
-    <h1>Education-Enhanced</h1>
-    <p>Use the shortcut: <b>Ctrl + shift + ~</b> in order to activate extension</p>
-    <br>
-    <button id="myButton">I'm a button</button>
-    <script src="inlineTest.js"></script>
+  <div class="screen" id="index">
+    
+    <div class="nav">
+      <ul>
+        <li><p>EE</p></li>
+        <li><a href="#index">Home</a></li>
+        <li><a href="#calculator">Calculator</a></li>
+        <li><a href="#formulas">Formulas</a></li>
+      </ul>
+    </div>
+    
+    <div class="content">
+      <h1>Home</h1>
+      <p>This is a tool for helping yourself and your education. If you want or need a feature to be added ... </p>
+    </div>
+
+    
+  </div>
+  <!-- CALCULATOR -->
+  <div class="screen" id="calculator">
+
+    <div class="nav">
+      <ul>
+        <li><p>EE</p></li>
+        <li><a href="#index">Home</a></li>
+        <li><a href="#calculator">Calculator</a></li>
+        <li><a href="#formulas">Formulas</a></li>
+      </ul>
+    </div>
+
+    <div class="content">
+      <div class="calcNav">
+        <h1>Calculator</h1>
+        <ul>
+          <li><a href="calculatorScientific.html">Scientific Calculator</a></li>
+          <li><a href="calculatorGraphing.html">Graphing Calculator</a></li>
+          <li><p><br>These calculators have been imported from <a href="https://www.desmos.com/scientific" target="_blank">desmos.com/scientific</a> and <br><a href="https://www.desmos.com/calculator" target="_blank">desmos.com/calculator</a>.</p></li>
+        </ul>
+      </div>
+    </div>
+
+
+  </div>
+<!-- FORMULAS -->
+  <div class="screen" id="formulas">
+
+    <div class="nav">
+      <ul>
+        <li><p>EE</p></li>
+        <li><a href="#index">Home</a></li>
+        <li><a href="#calculator">Calculator</a></li>
+        <li><a href="#formulas">Formulas</a></li>
+      </ul>
+    </div>
+
+    <div class="content">
+        <h1>Formulas - <a href="https://www.matematica.pt/en/useful/math-formulas.php" target="_blank">Visit</a></h1>
+        <iframe src="https://www.matematica.pt/en/useful/math-formulas.php" width="100%" height="400px" frameborder="0" allowfullscreen name="Graphing Calculator"></iframe>
+    </div>
+
+
+  </div>
 </body>
+
 </html>
 `
 ;
@@ -26,7 +139,7 @@ window.addEventListener('keydown', (keyPressed) => {
     switch(keyPressed.key){
         case "~":
             if(map.Control === true && map.Shift === true){
-                var w = window.open('https://youtube.com', "ModWindow", "width=600, height=475, scrollbars=no");
+                var w = window.open('', "ModWindow", "width=600, height=475, scrollbars=no");
                 w.document.write(newHTML);
             }
             break;
