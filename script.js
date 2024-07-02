c9c1b2e694e59849dec1ff14723a6b2e6cc866a9
SuperMeerkat345 <artiom22903@gmail.com>
2024-07-02 16:42:42 -0400

    tried and failed to implement activeTab so we're going with matches since i can't specify window height/width with activeTab

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 var map = {};
 
-
-
 window.addEventListener('keydown', (keyPressed) => {
     switch(keyPressed.key){
         case "~":
             if(map.Control === true && map.Shift === true){
                 var w = window.open('http://education-enhanced.rf.gd/index.html', "ModWindow", "width=600, height=485, scrollbars=no");
-                
             }
             break;
         case "Control":
